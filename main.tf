447393350342b31c77bbc9da96dfe9a64169e3f2
Renat <rzhilkibaev@gmail.com>
2017-04-08 20:58:40 -0700

    Initial commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+# Configures SNS as Lambda event source 
+
+variable "topic_arn" {}
+variable "function_arn" {}
+
+resource "aws_lambda_permission" "allow_invocation_from_sns" {
+  function_name = "${var.function_arn}"
+  statement_id = "allow_invocation_from_sns"
+  action = "lambda:InvokeFunction"
+  principal = "sns.amazonaws.com"
+  source_arn = "${var.topic_arn}"
+}
+
+resource "aws_sns_topic_subscription" "invoke_lambda_on_topic_event" {
+    topic_arn = "${var.topic_arn}"
+    protocol = "lambda"
+    endpoint = "${var.function_arn}"
+}
+
