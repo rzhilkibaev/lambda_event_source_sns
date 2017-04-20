2783deca75f1eb20f3fb5a8af193823574a3b937
Renat <rzhilkibaev@gmail.com>
2017-04-20 01:53:44 -0700

    Formatting, put primary resource first

--- main.tf
+++ main.tf
@@ -4,17 +4,17 @@ variable "topic_arn" {}
 variable "function_arn" {}
 variable "permission_statement_id" { default = "allow_invocation_from_sns" }
 
+resource "aws_sns_topic_subscription" "invoke_lambda_on_topic_event" {
+    topic_arn = "${var.topic_arn}"
+    protocol = "lambda"
+    endpoint = "${var.function_arn}"
+}
+
 resource "aws_lambda_permission" "allow_invocation_from_sns" {
   function_name = "${var.function_arn}"
   statement_id = "${var.permission_statement_id}"
   action = "lambda:InvokeFunction"
   principal = "sns.amazonaws.com"
   source_arn = "${var.topic_arn}"
-}
-
-resource "aws_sns_topic_subscription" "invoke_lambda_on_topic_event" {
-    topic_arn = "${var.topic_arn}"
-    protocol = "lambda"
-    endpoint = "${var.function_arn}"
 }
 
